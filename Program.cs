4bebf9342bfb5dbdb0c241b2cd4d9da7faf7eebb
Landon Peterson <landon.peterson2@gmail.com>
2026-05-20 18:01:29 -0600

    work with doubles

--- Program.cs
+++ Program.cs
@@ -1,20 +1,31 @@
 ﻿//WorkWithIntegers();
-OrderPrecedence();
+//OrderPrecedence();
+WorkWithDoubles();
 
-int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine($"quotient: {d}");
-Console.WriteLine($"remainder: {e}");
 
-int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
 
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+
+void WorkWithDoubles()
+{
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+double a2 = 450.0;
+double b2 = 950.0;
+double c2 = 1750.0;
+double d2 = a2 * b2 / c2;
+Console.WriteLine(d2);
+}
 
 void WorkWithIntegers()
 {
@@ -53,4 +64,19 @@ int f = 4;
 int g = 3;
 int h = (e + f) / g;
 Console.WriteLine(h);
+
+int a2 = 7;
+int b2 = 4;
+int c2 = 3;
+int d2 = (a2 + b2) / c2;
+int e2 = (a2 + b2) % c2;
+Console.WriteLine($"quotient: {d2}");
+Console.WriteLine($"remainder: {e2}");
+
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
 }
