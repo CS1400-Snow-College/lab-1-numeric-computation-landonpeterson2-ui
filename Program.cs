94a393cb50b54e044b50998465e45e28ab20a3a0
Landon Peterson <landon.peterson2@gmail.com>
2026-05-20 18:16:21 -0600

    circle equation done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 ﻿//WorkWithIntegers();
 //OrderPrecedence();
-WorkWithDoubles();
+//WorkWithDoubles();
 
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+double e = 2.50;
+double f = Math.PI * e * e;
+Console.WriteLine(f);
 
 
 
